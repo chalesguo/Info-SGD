e7a14cc06d47143ef8942e3917131e3cc01a6332
chalesguo <guo.chsh@gmail.com>
2021-05-08 10:57:39 +0800

    Add files via upload

---
+++ run_googlenet.sh
@@ -0,0 +1,31 @@
+python main.py --nr 0.00  --model googlenet
+
+python mainw.py --nr 0.02   --model googlenet --mode zs  --dim 0
+python mainw.py --nr 0.02   --model googlenet --mode zs  --dim 1
+python mainw.py --nr 0.02   --model googlenet --mode pj  --dim 0
+python mainw.py --nr 0.02   --model googlenet --mode pj  --dim 1
+
+python mainw.py --nr 0.04   --model googlenet --mode zs  --dim 0
+python mainw.py --nr 0.04   --model googlenet --mode zs  --dim 1
+python mainw.py --nr 0.04   --model googlenet --mode pj  --dim 0
+python mainw.py --nr 0.04   --model googlenet --mode pj  --dim 1
+
+python mainw.py --nr 0.06   --model googlenet --mode zs  --dim 0
+python mainw.py --nr 0.06   --model googlenet --mode zs  --dim 1
+python mainw.py --nr 0.06   --model googlenet --mode pj  --dim 0
+python mainw.py --nr 0.06   --model googlenet --mode pj  --dim 1
+
+python mainw.py --nr 0.08   --model googlenet --mode zs  --dim 0
+python mainw.py --nr 0.08   --model googlenet --mode zs  --dim 1
+python mainw.py --nr 0.08   --model googlenet --mode pj  --dim 0
+python mainw.py --nr 0.08   --model googlenet --mode pj  --dim 1
+
+python mainw.py --nr 0.10   --model googlenet --mode zs  --dim 0
+python mainw.py --nr 0.10   --model googlenet --mode zs  --dim 1
+python mainw.py --nr 0.10   --model googlenet --mode pj  --dim 0
+python mainw.py --nr 0.10   --model googlenet --mode pj  --dim 1
+
+python mainw.py --nr 0.12   --model googlenet --mode zs  --dim 0
+python mainw.py --nr 0.12   --model googlenet --mode zs  --dim 1
+python mainw.py --nr 0.12   --model googlenet --mode pj  --dim 0
+python mainw.py --nr 0.12   --model googlenet --mode pj  --dim 1
